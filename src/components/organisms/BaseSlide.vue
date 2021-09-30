39068b651b58e02c7992503970df87a387c18470
Michal-Dziedzinski <michal.marcin.dziedzinski@gmail.com>
2021-09-30 13:03:48 +0200

    add components

---
+++ src/components/organisms/BaseSlide.vue
@@ -0,0 +1,20 @@
+<template>
+	<ArrowButton />
+	<BaseImage />
+	<ArrowButton />
+</template>
+
+<script>
+import BaseImage from '@/components/molecules/BaseImage.vue';
+import ArrowButton from '@/components/atoms/ArrowButton.vue';
+
+export default {
+	name: 'BaseSlide',
+	components: {
+		BaseImage,
+		ArrowButton,
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
